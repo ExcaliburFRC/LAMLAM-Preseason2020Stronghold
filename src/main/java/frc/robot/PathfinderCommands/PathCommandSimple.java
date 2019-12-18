51f659eda55f03946a89ef2ab44d7a04e84650d2
Starlight <yotamshlomi@gmail.com>
2019-12-18 21:41:06 +0200

    Fixed lib problems

--- src/main/java/frc/robot/PathfinderCommands/PathCommandSimple.java
+++ src/main/java/frc/robot/PathfinderCommands/PathCommandSimple.java
@@ -1,4 +1,4 @@
-package frc.robot.XcaLIB.Singletons;
+package frc.robot.PathfinderCommands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
@@ -15,24 +15,24 @@ public class PathCommandSimple extends Command {
     private static final double k_max_velocity = 1.7;
     private static final double P = 1, D = 0.1, kV = 1 / k_max_velocity, k_gp = 1;
 
-    Trajectory left_traj, right_traj;
-    EncoderFollower left, right;
+    private Trajectory left_trajectory, right_trajectory;
+    private EncoderFollower left, right;
 
     public PathCommandSimple(String filepath) {
         try {
-            right_traj = PathfinderFRC.getTrajectory(filepath + ".right.pf1.csv");//reads from file
-            left_traj = PathfinderFRC.getTrajectory(filepath + ".left.pf1.csv");
+            right_trajectory = PathfinderFRC.getTrajectory(filepath + ".right.pf1.csv");//reads from file
+            left_trajectory = PathfinderFRC.getTrajectory(filepath + ".left.pf1.csv");
         } catch (IOException e) {
             System.out.println("file doesnt exist. Set one and download ASAP.");
             System.out.println("Original Error Message : " + e.getMessage());
         }
 
-        left = new EncoderFollower(left_traj);
-        right = new EncoderFollower(right_traj);
+        left = new EncoderFollower(left_trajectory);
+        right = new EncoderFollower(right_trajectory);
 
-        left.configureEncoder(Robot.m_chassi.getLeftEncVal(), k_ticks_per_rev,
+        left.configureEncoder(Robot.m_chassi.getLeftEncoder(), k_ticks_per_rev,
                 k_wheel_diameter);
-        right.configureEncoder(Robot.m_chassi.getLeftEncVal(), k_ticks_per_rev,
+        right.configureEncoder(Robot.m_chassi.getRightEncoder(), k_ticks_per_rev,
                 k_wheel_diameter);
 
         left.configurePIDVA(P, 0, D, kV, 0);
@@ -47,11 +47,11 @@ public class PathCommandSimple extends Command {
         double desired_heading = Pathfinder.r2d(left.getHeading());
 
         //TODO("check gyro val handling")
-        double turn = k_gp * (desired_heading - Robot.m_chassi.gyro.getAngle());
+        double turn = k_gp * (desired_heading - Robot.m_chassi.getGyroValue());
 
 
-        Robot.m_chassi.setLeft(left_speed + turn);
-        Robot.m_chassi.setRight(right_speed - turn);
+        Robot.m_chassi.getDrive().tankDrive(left_speed + turn, right_speed - turn);
+        
     }
 
     @Override
