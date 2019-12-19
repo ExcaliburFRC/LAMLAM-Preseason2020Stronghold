d233ad1bb3ee98fe7efbf026408452056fcabee7
renderhaf <dvir.akerman@gmail.com>
2019-12-19 12:45:49 +0200

    SomeChanges to the motion profiling

--- src/main/java/frc/robot/PathfinderCommands/PathCommandSimple.java
+++ src/main/java/frc/robot/PathfinderCommands/PathCommandSimple.java
@@ -10,10 +10,10 @@ import jaci.pathfinder.followers.EncoderFollower;
 import java.io.IOException;
 
 public class PathCommandSimple extends Command {
-    private static final int k_ticks_per_rev = 1024;
-    private static final double k_wheel_diameter = 4.0 / 12.0;
-    private static final double k_max_velocity = 1.7;
-    private static final double P = 1, D = 0.1, kV = 1 / k_max_velocity, k_gp = 1;
+    private static final int k_ticks_per_rev = 360;
+    private static final double k_wheel_diameter = 15.24;
+    // private static final double k_max_velocity = 2.5;
+    private static final double kP = 1, kD = 0.1, kV = 0, kA = 0, k_gp = 0.8;
 
     private Trajectory left_trajectory, right_trajectory;
     private EncoderFollower left, right;
@@ -35,8 +35,14 @@ public class PathCommandSimple extends Command {
         right.configureEncoder(Robot.m_chassi.getRightEncoder(), k_ticks_per_rev,
                 k_wheel_diameter);
 
-        left.configurePIDVA(P, 0, D, kV, 0);
-        right.configurePIDVA(P, 0, D, kV, 0);
+        left.configurePIDVA(kP, 0, kD, kV, kA);
+        right.configurePIDVA(kP, 0, kD, kV, kA);
+    }
+
+    @Override
+    protected void initialize() {
+        left.reset();
+        right.reset();
     }
 
     @Override
@@ -44,18 +50,26 @@ public class PathCommandSimple extends Command {
         double left_speed = left.calculate(Robot.m_chassi.getLeftEncoder());
         double right_speed = right.calculate(Robot.m_chassi.getRightEncoder());
 
+        double heading = Robot.m_chassi.getGyroValue();
         double desired_heading = Pathfinder.r2d(left.getHeading());
-
-        //TODO("check gyro val handling")
-        double turn = k_gp * (desired_heading - Robot.m_chassi.getGyroValue());
-
+        double heading_difference = Pathfinder.boundHalfDegrees(desired_heading - heading);
+        double turn =  k_gp * (-1.0/80.0) * heading_difference;
 
         Robot.m_chassi.getDrive().tankDrive(left_speed + turn, right_speed - turn);
-        
     }
 
     @Override
     protected boolean isFinished() {
-        return (left.isFinished() || right.isFinished());
+        return (left.isFinished() && right.isFinished());
+    }
+
+    @Override
+    protected void end() {
+        Robot.m_chassi.getDrive().tankDrive(0,0);
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
     }
 }
